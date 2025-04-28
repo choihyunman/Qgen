151ef302a80b8dc4a69ce46f2592ff415cd95afb
홍정표 <hsc3757@gmail.com>
2025-04-29 00:14:38 +0900

    :sparkles: feat: ERD에 따른 JPA Entity 설계 및 클래스 작성

---
+++ backend/src/main/java/com/s12p31b204/backend/domain/User.java
@@ -0,0 +1,45 @@
+package com.s12p31b204.backend.domain;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@EntityListeners(AuditingEntityListener.class)
+public class User {
+
+    @Id
+    @EqualsAndHashCode.Include
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long userId;
+
+    @NonNull
+    @Column(nullable = false, length = 30, unique = true)
+    private String googleEmail;
+
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<WorkBook> workBooks;
+
+    @CreatedDate
+    private LocalDateTime createAt;
+}
