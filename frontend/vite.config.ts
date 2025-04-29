b3b909d05f6aa987efaa682dcfcd0e67ec107c71
cchh6462 <cchh6462@naver.com>
2025-04-29 04:31:14 +0000

    :rocket: Chore: 프론트엔드 config 수정

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -13,6 +13,6 @@ export default defineConfig({
     watch: {
       usePolling: true
     },
-    allowedHosts: ['q-generator.com', 'localhost']
+    allowedHosts: ['q-generator.com', 'localhost', 'frontend']
   }
 })
