2f59297979d0aafc98e2510da31a289e9e8de41e
Jaidy <hyjgood2000@naver.com>
2025-04-30 17:18:24 +0900

    📝 feat : layout 설정 (Header, Footer)

--- frontend/src/components/layout/Header/Header.tsx
+++ frontend/src/components/layout/Header/Header.tsx
@@ -5,7 +5,7 @@ export default function Header() {
     <header className='w-full flex items-center justify-between px-8 py-4 bg-white rounded-full shadow-[0_0_24px_0_rgba(0,0,0,0.08)]'>
       <span className='text-3xl font-bold'>Q-gen</span>
       <div className='flex items-center gap-3'>
-        <a className='text-base font-medium cursor-pointer'>문제집 바로가기</a>
+        <a className='text-base font-medium '>문제집 바로가기</a>
         <SmallBtn text='로그인' />
         <SmallBtn text='가입' outline />
       </div>
