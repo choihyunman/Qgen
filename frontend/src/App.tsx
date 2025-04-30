8fca535929aed9827f041d76beae463d052516b6
Jaidy <hyjgood2000@naver.com>
2025-04-30 09:56:17 +0900

    feat : layout Component Header, Footer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,10 @@
 import './App.css'
-import SmallBtn from './components/SmallBtn/SmallBtn'
 
 function App() {
 
   return (
     <div className="">
       메인 화면 입니다.
-      <SmallBtn text="로그인" />
     </div>
   )
 }
---
+++ frontend/src/components/layout/Header/Header.tsx
@@ -0,0 +1,14 @@
+import SmallBtn from "@/components/SmallBtn/SmallBtn";
+
+export default function Header() {
+  return (
+    <header className="w-full flex items-center justify-between px-8 py-4 bg-white rounded-full shadow-[0_0_24px_0_rgba(0,0,0,0.25)]">
+      <span className="text-3xl font-bold">Q-gen</span>
+      <div className="flex items-center gap-3">
+        <span className="text-base font-medium">문제집 바로가기</span>
+        <SmallBtn text="로그인" />
+        <SmallBtn text="가입" outline />
+      </div>
+    </header>
+  );
+}
