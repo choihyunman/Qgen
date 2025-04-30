7ab5473b51711ead4969c9b47b1fd64d7eaca15c
Jaidy <hyjgood2000@naver.com>
2025-04-30 17:05:23 +0900

    📝 feat : layout 설정 (Header, Footer)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,27 @@
 import './App.css';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import Header from './components/layout/Header/Header';
 import Footer from './components/layout/Footer/Footer';
+import ArcBackground from './components/layout/Background/ArcBackground';
+import BlurBackground from './components/layout/Background/BlurBackground';
 
 function App() {
+  const location = useLocation();
+
+  // 예시: /special로 시작하면 ArcBackground, 아니면 BlurBackground
+  const isArcPage = location.pathname.startsWith('/quiz');
+  const BackgroundComponent = isArcPage ? ArcBackground : BlurBackground;
+
   return (
-    <div className='main-container'>
-      <Header />
-      <Outlet />
-      <Footer />
-    </div>
+    <BackgroundComponent>
+      <div className='main'>
+        <Header />
+        <main>
+          <Outlet />
+        </main>
+        <Footer />
+      </div>
+    </BackgroundComponent>
   );
 }
 
