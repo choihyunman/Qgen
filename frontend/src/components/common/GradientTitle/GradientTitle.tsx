47f1361a5b22e00e4246a1bf9e48095472f6b715
Jaidy <hyjgood2000@naver.com>
2025-05-14 14:21:22 +0900

    🛠️fix : 수정사항 반영

--- frontend/src/components/common/GradientTitle/GradientTitle.tsx
+++ frontend/src/components/common/GradientTitle/GradientTitle.tsx
@@ -12,7 +12,7 @@ export default function GradientTitle({
   className,
 }: GradientTitleProps) {
   return (
-    <h2 className={` font-extrabold flex items-end ${className ?? ''}`}>
+    <h2 className={`text-4xl font-extrabold flex items-end ${className ?? ''}`}>
       {before && <span className='text-black '>{before}</span>}
       <span className=' bg-gradient-to-r from-[#6D6DFF] to-[#B16DFF] text-transparent bg-clip-text px-1'>
         {highlight}
