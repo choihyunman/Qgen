de92b44c9736ed350c683d10ff480f89eb0ebd87
choihyunman <cchh6462@naver.com>
2025-05-16 16:13:34 +0900

    :rocket: Chore: 디버깅을 위한 로그 추가

--- ai/app/server.py
+++ ai/app/server.py
@@ -3,6 +3,11 @@ from app.problem_generator.chatgpt import router as chatgpt_router
 from app.health_check.health_check import router as health_router
 from app.rag.router import router as rag_router
 
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+
 app = FastAPI()
 
 app.include_router(health_router, prefix="/api/ai")
