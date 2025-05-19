54cfdc995a0df970682f0a7d74e9409a57090372
Sin Uk <lsu0930@gmail.com>
2025-05-19 11:18:05 +0900

    :lipstick: Design: 업로드된 자료 높이 조정

--- frontend/src/pages/List/List.tsx
+++ frontend/src/pages/List/List.tsx
@@ -434,7 +434,7 @@ export default function List() {
       {/* 문제집 & 자료 업로드 */}
       <section className='flex gap-8'>
         {/* 문제집 리스트 */}
-        <div className='flex-1 flex flex-col gap-0 '>
+        <div className='flex-1 flex flex-col gap-0 h-full'>
           {/* 제목 파트 */}
           <div className='flex justify-between pt-4 pb-3 items-center'>
             <div className='flex items-center gap-2 h-[40px]'>
@@ -535,8 +535,8 @@ export default function List() {
             </div>
           </div>
 
-          <div className='flex gap-5'>
-            <div className='flex-4'>
+          <div className='flex gap-5 items-stretch h-full'>
+            <div className='flex-4 h-full'>
               {/* 로딩/에러 처리 */}
               {isLoading && <div>로딩 중...</div>}
               {error && <div className='text-red-500'>{error.message}</div>}
@@ -594,12 +594,13 @@ export default function List() {
             </div>
             {/* 자료 업로드 - selectedWorkbook이 있을 때만 표시 */}
             {numericWorkBookId && (
-              <aside className='flex flex-2 shrink-0'>
+              <aside className='flex flex-2 shrink-0 h-full'>
                 <UploadedList
                   files={files}
                   maxFiles={30}
                   onDelete={handleDelete}
                   onClick={() => setIsUploadModalOpen(true)}
+                  className='h-full'
                 />
                 <UploadModal
                   isOpen={isUploadModalOpen}
